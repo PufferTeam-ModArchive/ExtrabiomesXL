c9e567d9599fdbe8aed3a8dbd82dc8e9d21a7d6c
jss2a98aj <jsimmons3959@neo.rr.com>
2021-12-26 18:30:35 -0500

    Check for and use Et Futurum dyes if self added dyes are disabled

--- src/main/java/extrabiomes/handlers/ItemHandler.java
+++ src/main/java/extrabiomes/handlers/ItemHandler.java
@@ -6,8 +6,6 @@
 
 package extrabiomes.handlers;
 
-import net.minecraft.item.ItemStack;
-
 import com.google.common.base.Optional;
 
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -20,49 +18,61 @@ import extrabiomes.lib.Element;
 import extrabiomes.lib.ItemSettings;
 import extrabiomes.lib.ModuleControlSettings;
 import extrabiomes.lib.Reference;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
-public abstract class ItemHandler
-{
-    
-    public static void createItems()
-    {
-        createLogTurner();
+public abstract class ItemHandler {
+
+	public static void createItems() {
+		createLogTurner();
 		createDye();
 		createFoods();
-    }
-    
-    private static void createLogTurner()
-    {
-        if (!ModuleControlSettings.SUMMA.isEnabled() || !ItemSettings.LOGTURNER.getEnabled())
-            return;
-        
-        final LogTurner logTurner = new LogTurner();
-        
-        Stuff.logTurner = Optional.of(logTurner);
-        
-        logTurner.setUnlocalizedName("extrabiomes.logturner").setCreativeTab(Extrabiomes.tabsEBXL);
-        
-        GameRegistry.registerItem(logTurner, "extrabiomes.logturner", Reference.MOD_ID);
-        
-        Element.LOGTURNER.set(new ItemStack(logTurner));
-    }
-    
-	private static void createDye() {
-		if (!ItemSettings.DYE.getEnabled()) return;
-
-		final ItemCustomDye dye = new ItemCustomDye();
-		Stuff.dye = Optional.of(dye);
-		dye.setUnlocalizedName("extrabiomes.dye").setCreativeTab(Extrabiomes.tabsEBXL);
-		GameRegistry.registerItem(dye, "extrabiomes.dye", Reference.MOD_ID);
-
-		dye.init();
 	}
-	
+
+	private static void createLogTurner() {
+		if (!ModuleControlSettings.SUMMA.isEnabled() || !ItemSettings.LOGTURNER.getEnabled()) {
+			return;
+		}
+
+		final LogTurner logTurner = new LogTurner();
+
+		Stuff.logTurner = Optional.of(logTurner);
+
+		logTurner.setUnlocalizedName("extrabiomes.logturner").setCreativeTab(Extrabiomes.tabsEBXL);
+
+		GameRegistry.registerItem(logTurner, "extrabiomes.logturner", Reference.MOD_ID);
+
+		Element.LOGTURNER.set(new ItemStack(logTurner));
+	}
+
+	private static void createDye() {
+		if (!ItemSettings.DYE.getEnabled()) {
+			final String dyetype = "etfuturum:dye";
+			if (Item.itemRegistry.containsKey(dyetype)) {
+				final Item dye = (Item)Item.itemRegistry.getObject(dyetype);
+				Element.DYE_BLACK.set(new ItemStack(dye, 1, 3));
+				Element.DYE_BLUE.set(new ItemStack(dye, 1, 1));
+				Element.DYE_BROWN.set(new ItemStack(dye, 1, 2));
+				Element.DYE_WHITE.set(new ItemStack(dye, 1, 0));
+			}
+		} else {
+			final ItemCustomDye dye = new ItemCustomDye();
+			Stuff.dye = Optional.of(dye);
+			dye.setUnlocalizedName("extrabiomes.dye").setCreativeTab(Extrabiomes.tabsEBXL);
+			GameRegistry.registerItem(dye, "extrabiomes.dye", Reference.MOD_ID);
+
+			dye.init();
+		}
+	}
+
 	private static void createFoods() {
-		if(!ItemSettings.FOOD.getEnabled()) return;
-		
+		if(!ItemSettings.FOOD.getEnabled()) {
+			return;
+		}
+
 		final ItemCustomFood food = new ItemCustomFood();
 		Stuff.food = Optional.of(food);
 		GameRegistry.registerItem(food, "extrabiomes.food", Reference.MOD_ID);
 	}
+
 }
